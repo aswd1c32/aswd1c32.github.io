f294550898eb14c46317b5cba4dbfcfdd054fbac
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2022-09-15 14:40:19 -0500

    give js example
    e

---
+++ example.js
@@ -0,0 +1,25 @@
+// Variables store data
+// the let keyword means it can be changed, the var keyword means you are old, and the const keyword means that the data cannot be changed
+// there are a lot of data types
+let booleanExample = false; // true or false
+let numberExample = 420.69; // signed integer or decimal
+let stringExample = "amogus"; // text
+let arrayExample = [false, stringExample, 420.69]; // list; variable names can be used in place of values
+// arrayExample[0] is the first element, arrayExample[1] is the second, ...
+let objectExample = {
+  arr: arrayExample,
+  "qué": 6
+};
+// objectExample.arr is the same as objectExample["arr"] and they are both the array
+// logic is useful
+// the if statement checks if something is true
+// ! means not
+// && means and
+// || means or
+// == means is
+// != means is not
+// < and > are what they mean in math
+if(3 > 2)
+{
+  // math works
+}
