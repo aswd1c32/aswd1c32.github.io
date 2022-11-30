34833fe00675eec3e700fa7d7bd14b5d99a79a54
Ava Myers <113209637+aswd1c32@users.noreply.github.com>
2022-11-29 20:27:42 -0600

    Update stuff.js

--- stuff.js
+++ stuff.js
@@ -1,5 +1,5 @@
 function rickroll() { // I suggest naming functions something that describes what it does.
-        location.replace("https://www.youtu.be/dQw4w9WgXcQ"); /* Inline JavaScript? Not a good practice.
+        location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); /* Inline JavaScript? Not a good practice.
                                                                  Instead, use a src property to link to a separate JavaScript file.
                                                                  Ex. <script src="file.js"><(slash)script>                         */
       }
