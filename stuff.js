e18a92deff9aad50261608566d4c6b63db1856f5
jbxghexkojdv <107658001+jbxghexkojdv@users.noreply.github.com>
2023-03-07 08:01:27 -0600

    Clean up code
    Clean up the one function in this file lol
    the whitespace was goofy

--- stuff.js
+++ stuff.js
@@ -1,4 +1,3 @@
-function rickroll() { // I suggest naming functions something that describes what it does.
-        location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ"); 
-        
-         }
+function rickroll() {
+  location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
