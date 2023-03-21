7e888a2e398e26e3acbe02792bb4103d4818edc9
jbxghexkojdv <jbxghexkojdv@gmail.com>
2023-03-21 16:30:40 -0500

    Minor changes
    Corrected the grammar in the first paragraph, add a few comments

--- stuff.js
+++ stuff.js
@@ -1,3 +1,6 @@
 function rickroll() {
   location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 }
+// Maybe you shuld put some stuff here.
+// Oh, and by "some," I mean a lot.
+// 86.9% of my website is JS. - jbxghexkojdv
