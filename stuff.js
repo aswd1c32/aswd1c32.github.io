a17df4ee5f6e2a5fc3913661429a55127dee8056
jbxghexkojdv <jbxghexkojdv@gmail.com>
2023-03-28 23:54:22 -0500

    Add Spanish and French translations
    that took forever lol

--- stuff.js
+++ stuff.js
@@ -1,6 +1,138 @@
 function rickroll() {
   location.replace("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 }
-// Maybe you shuld put some stuff here.
-// Oh, and by "some," I mean a lot.
-// 86.9% of my website is JS. - jbxghexkojdv
+const onLocal = false;
+// à -> U+00E0
+// á -> U+00E1
+// ç -> U+00E7
+// è -> U+00E8
+// é -> U+00E9
+// ê -> U+00EA
+// í -> U+00ED
+// î -> U+00EE
+// ñ -> U+00F1
+// ó -> U+00F3
+// ô -> U+00F4
+// ù -> U+00F9
+// ú -> U+00FA
+// û -> U+00FB
+// ü -> U+00FC
+// ¿ -> U+00BF
+// ¡ -> U+00A1
+function translate(lang)
+{
+  switch(lang)
+  {
+    case "es":
+      document.body.parentElement.lang = "es";
+      history.replaceState(null, null, onLocal ? "./index.html" : "./");
+      document.getElementsByTagName("label")[0].innerHTML = "Nombre (4 a 8 car&#xE1;cteres y tambi&#xE9;n no importante):";
+      document.getElementById("website-desc").innerHTML = 
+      "Hola, este sitio web es para un proyecto escuela, pero tambi&#xE9;n hago esto por diversi&#xF3;n.\
+      Me interesa programar y tengo la ayuda de mi buen amigo,\
+      jbxghexkojdv en GitHub, y tambi&#xE9;n lo conozco en vida real.";
+      document.title = "El sitio web de Aswd1c32";
+      document.getElementById("header-before-links").innerHTML = "Un sitio web para cosas miscel&#xE1;neas";
+      document.getElementById("list-of-links").children[0].children[0].innerHTML = "El sitio web del cocreador";
+      document.getElementById("list-of-links").children[2].children[0].innerHTML = "Mi perfil Roblox";
+      document.getElementById("list-of-links").children[4].children[0].innerHTML = "Mi perfil GitHub";
+      document.getElementById("list-of-links").children[5].children[0].innerHTML = "Fortnite helic&#xF3;ptero";
+      document.getElementById("list-of-links").children[6].children[0].innerHTML = "Mi perfil Scratch";
+      document.getElementById("list-of-links").children[7].children[0].innerHTML = "Historia ocultada de Fortnite";
+      document.getElementById("list-of-links").children[8].children[0].innerHTML = "El wiki Amogus entero";
+      document.getElementById("list-of-links").children[9].children[0].innerHTML = "P&#xE1;gina Amogus";
+      document.getElementById("list-of-links").children[10].children[0].innerHTML = "Contactarme";
+      document.getElementById("list-of-links").children[12].children[0].innerHTML = "Aqu&#xED; est&#xE1; un link a OSU";
+      document.getElementById("list-of-links").children[13].children[0].innerHTML = "Puerta";
+      document.getElementById("list-of-links").children[14].children[0].innerHTML = "W3Schools, el sitio web que uso para obtener mis tutoriales HTML";
+      document.getElementById("list-of-links").children[15].children[0].innerHTML = "Documentaci&#xF3;n HTML MDN - A&#xF1;adido por jbxghexkojdv";
+      document.getElementById("detotaded-wam").innerHTML = "&#xBF;Cu&#xE1;nto WAM deditado necesito para una sewvidow Minecwaf?";
+      document.getElementById("tcalc-desc").innerHTML = "No, estoy en TCALC, hermano.";
+      document.getElementById("doors-desc").innerHTML = "Car&#xE1;cteres de Doors Roblox favoritos";
+      document.getElementById("roblox-doors-characters").children[0].children[0].children[0].innerHTML = "Car&#xE1;cter";
+      document.getElementById("roblox-doors-characters").children[0].children[0].children[1].innerHTML = "Rango";
+      document.getElementById("tie-rank-desc").innerHTML = "Corbatas favoritas";
+      document.getElementById("tie-ranking").children[0].children[0].children[0].innerHTML = "Corbata";
+      document.getElementById("tie-ranking").children[0].children[0].children[1].innerHTML = "Rango";
+      document.getElementById("tie-ranking").children[0].children[1].children[0].innerHTML = "Flores blancas";
+      document.getElementById("tie-ranking").children[0].children[2].children[0].innerHTML = "Flores moradas";
+      document.getElementById("tie-ranking").children[0].children[3].children[0].innerHTML = "A rayas";
+      document.getElementById("fortnite-win-desc").innerHTML = "Mis victorias Fortnite";
+      document.getElementById("fortnite-wins").children[0].children[0].children[0].innerHTML = "Modo";
+      document.getElementById("fortnite-wins").children[0].children[0].children[1].innerHTML = "Victorias";
+      document.getElementById("fortnite-wins").children[0].children[2].children[0].innerHTML = "D&#xFA;os";
+      document.getElementById("fortnite-wins").children[0].children[3].children[0].innerHTML = "Tr&#xED;os";
+      document.getElementById("fortnite-wins").children[0].children[4].children[0].innerHTML = "Equipos";
+      document.getElementById("lack-of-ideas").innerHTML = "No s&#xE9; realmente qu&#xE9; hacer con esta sitio web.";
+      document.getElementById("rickroll-button").innerHTML = "&#xA1;YOOO! &#xA1;HAZ CLIC EN ESTO!";
+      document.getElementById("carrot-desc").innerHTML = "Unas zanahorias que encontr&#xE9; en Google";
+      document.getElementById("me-desc").innerHTML = "Yo (el creador m&#xE1;s importante)";
+      document.getElementById("coconutmall-desc").innerHTML = "Gata llamada por pista de Mario Kart, Coconut Mall.";
+      document.getElementById("coco-desc").innerHTML = "Coco, odia ser tomada, y te ara&#xF1;a si la tomas.";
+      document.getElementById("buddy-desc").innerHTML = "Buddy, un perro precioso. Me mordi&#xF3; en la cara, y ahora tengo un cicatriz en la mejilla.";
+      document.getElementById("pangolin-desc").innerHTML = "Los pangol&#xED;nes son preciosos, y Amogus.";
+      document.getElementById("god-desc").innerHTML = "Este es George Washington Carver, es mi Dios y salvador porque hizo cosas con cacahuates, y amo los cacahuates.";
+      document.getElementById("thanks-for-visiting").innerHTML = "&#xA1;Gracias por visitar mi sitio web! preferir&#xED;a alg&#xFA;n feedback sobre ello, as&#xED; que puedo hacerlo mejor. :)";
+      document.getElementById("credits").innerHTML = "Todo por Ava Myers, excepto: <br />Traducciones por Benjamin Harris (jbxghexkojdv)";
+      break;
+    case "fr":
+      document.body.parentElement.lang = "fr";
+      history.replaceState(null, null, onLocal ? "./index.html" : "./");
+      document.getElementsByTagName("label")[0].innerHTML = "Nom (4 &#xE0; 8 caract&#xE8;res et aussi pas important)&nbsp;:";
+      document.getElementById("website-desc").innerHTML = 
+      "Bonjour, ce site web est pour un school projet, mais je aussi fais &#xE7;a pour divertissement.\
+      M'int&#xE9;resse coder et j'ai l'aide de mon bon ami,\
+      jbxghexkojdv sur GitHub, et j'aussi le connais dans la r&#xE9;alit&#xE9;.";
+      document.title = "Le site web de Aswd1c32";
+      document.getElementById("header-before-links").innerHTML = "Un site web pour choses diverses";
+      document.getElementById("list-of-links").children[0].children[0].innerHTML = "Le site web du cocr&#xE9;ateur";
+      document.getElementById("list-of-links").children[2].children[0].innerHTML = "Mon profil Roblox";
+      document.getElementById("list-of-links").children[4].children[0].innerHTML = "Mon profil GitHub";
+      document.getElementById("list-of-links").children[5].children[0].innerHTML = "Fortnite h&#xE9;licopt&#xE8;re";
+      document.getElementById("list-of-links").children[6].children[0].innerHTML = "Mon profil Scratch";
+      document.getElementById("list-of-links").children[7].children[0].innerHTML = "Histoire cach&#xE9; de Fortnite";
+      document.getElementById("list-of-links").children[8].children[0].innerHTML = "Le wiki Amogus entier";
+      document.getElementById("list-of-links").children[9].children[0].innerHTML = "Page Amogus";
+      document.getElementById("list-of-links").children[10].children[0].innerHTML = "Me contacter";
+      document.getElementById("list-of-links").children[12].children[0].innerHTML = "Ici est un lien de OSU";
+      document.getElementById("list-of-links").children[13].children[0].innerHTML = "Porte";
+      document.getElementById("list-of-links").children[14].children[0].innerHTML = "W3Schools, le site web que j'utilise pour obtenir mes tutoriels";
+      document.getElementById("list-of-links").children[15].children[0].innerHTML = "Documentation HTML MDN - Ajout&#xE9; par jbxghexkojdv";
+      document.getElementById("detotaded-wam").innerHTML = "Combien WAM consacw&#xE9; j'ai besoin de pouw un sewveuw Minecwaf&nbsp;?";
+      document.getElementById("tcalc-desc").innerHTML = "Nan, je suis dans TCALC, mec.";
+      document.getElementById("doors-desc").innerHTML = "Caract&#xE8;res de Doors Roblox favoris";
+      document.getElementById("roblox-doors-characters").children[0].children[0].children[0].innerHTML = "Caract&#xE8;re";
+      document.getElementById("roblox-doors-characters").children[0].children[0].children[1].innerHTML = "Rang";
+      document.getElementById("tie-rank-desc").innerHTML = "Cravates favories";
+      document.getElementById("tie-ranking").children[0].children[0].children[0].innerHTML = "Cravate";
+      document.getElementById("tie-ranking").children[0].children[0].children[1].innerHTML = "Rang";
+      document.getElementById("tie-ranking").children[0].children[1].children[0].innerHTML = "Fleurs blanches";
+      document.getElementById("tie-ranking").children[0].children[2].children[0].innerHTML = "Fleurs violettes";
+      document.getElementById("tie-ranking").children[0].children[3].children[0].innerHTML = "Ray&#xE9;e";
+      document.getElementById("fortnite-win-desc").innerHTML = "Mes victoires Fortnite";
+      document.getElementById("fortnite-wins").children[0].children[0].children[0].innerHTML = "Mode";
+      document.getElementById("fortnite-wins").children[0].children[0].children[1].innerHTML = "Victoires";
+      document.getElementById("fortnite-wins").children[0].children[4].children[0].innerHTML = "&#xC9;quipes";
+      document.getElementById("lack-of-ideas").innerHTML = "Je ne sais plus vraiment quoi faire avec ce site web.";
+      document.getElementById("rickroll-button").innerHTML = "YOOO&nbsp;! CLIQUE SUR &#xC7;A&nbsp;!";
+      document.getElementById("carrot-desc").innerHTML = "Des carottes que j'ai trouv&#xE9; sur Google";
+      document.getElementById("me-desc").innerHTML = "Moi (le cr&#xE9;ateur plus important)";
+      document.getElementById("coconutmall-desc").innerHTML = "Chatte appel&#xE9;e d'apr&#xE8;s piste de Mario Kart, Coconut Mall.";
+      document.getElementById("coco-desc").innerHTML = "Coco, elle d&#xE9;teste &#xEA;tre prise, et elle t'&#xE9;gratigne si tu la prends.";
+      document.getElementById("buddy-desc").innerHTML = "Buddy, un chien mignon. Il m'a mordu sur le visage, et maintenant j'ai un cicatrice sur la joue.";
+      document.getElementById("pangolin-desc").innerHTML = "Les pangolins sont mignons, et Amogus.";
+      document.getElementById("god-desc").innerHTML = "C'est George Washington Carver, il est mon Dieu et sauveur parce qu'il a fait choses avec des cacahu&#xE8;tes, et j'aime les cacahu&#xE8;tes.";
+      document.getElementById("thanks-for-visiting").innerHTML = "Merci pour visiter mon site web! Je pr&#xE9;f&#xE9;rerais des r&#xE9;actions sur &#xE7;a, alors je peux le faire meilleur. :)";
+      document.getElementById("credits").innerHTML = "Tout par Ava Myers, sauf: <br />Tracuctions par Benjamin Harris (jbxghexkojdv)";
+      break;
+  }
+}
+
+if (location.search.match("lang=es"))
+{
+  translate("es");
+}
+else if (location.search.match("lang=fr"))
+{
+  translate("fr");
+}
